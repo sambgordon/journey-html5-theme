d4e5bb9d072a553deac8938ad5bb12740e24b2f5
DevBySam <samgordondev@gmail.com>
2017-12-26 18:55:14 -0700

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -4,7 +4,7 @@
 * Description: This is a static theme for developers and agencies.
 * Version: 1.0.0
 * Author: Sam Gordon
-* Author URI: http://devbysam.com 
+* Author URI: http://www.devbysam.com 
 * License: MIT
 */
 
